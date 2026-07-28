045ba80de615fe96d14fcea7cca03ce2fe3860d9
HiagoCS <hiago8cs@gmail.com>
2026-07-27 23:12:12 -0300

    banner default

--- frontend/src/components/banner/index.jsx
+++ frontend/src/components/banner/index.jsx
@@ -1,6 +1,18 @@
-function main(){
-    return(
-        <h1>Banner</h1>
+import './style.scss';
+function main() {
+    return (
+        <div className="app">
+            <div className="banner">
+                <div className="name">
+                    <h1>Hiago Costa Santos</h1>
+                    <h2>Desenvolvedor Full-Stack</h2>
+                </div>
+                <div className="logo">
+                    <text className='logo-text'>eight.cs</text>
+                    <text className='sublogo-text'>development</text>
+                </div>
+            </div>
+        </div>
     )
 }
 

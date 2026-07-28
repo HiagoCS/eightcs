845579f179d3ace3afa0974e4a8a96b4a026ce02
HiagoCS <hiago8cs@gmail.com>
2026-07-27 23:52:20 -0300

    svg icons for navbar

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Whatsapp from '@/assets/icons/whatsapp-svgrepo-com.svg?react';
+import Linkedin from '@/assets/icons/linkedin-svgrepo-com.svg?react';
+import Github from '@/assets/icons/github-142-svgrepo-com.svg?react';
+//
 import {data} from '@/data/links';
 
 import './style.scss';
@@ -6,12 +10,17 @@ function main(){
     return(
         <>
         <div className="main">
-            <ul>
+            <ul className='nav'>
                 {data.map((link) => (
                     <li key={link.url}>
                         <Link to={link.url}>{link['label']}</Link>
                     </li>
                 ))}
+            </ul>
+            <ul className='icons'>
+                <p><Whatsapp style={{width: '2.5pc', height: '2.5pc'}}/></p>
+                <p><Linkedin style={{width: '3.1pc', height: '3.1pc'}}/></p>
+                <p><Github style={{width: '2.5pc', height: '2.5pc'}}/></p>
             </ul>
         </div>
         </>

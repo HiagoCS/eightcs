11e41dea11a50a6a99ae30615897695bc73c4d66
HiagoCS <hiago8cs@gmail.com>
2026-08-07 16:39:16 -0300

    cards for SPA home

--- frontend/src/components/folders/home/index.jsx
+++ frontend/src/components/folders/home/index.jsx
@@ -6,7 +6,7 @@ import Cellphone from '@/assets/icons/cellphone.svg?react';
 function main() {
     return (
         <div className="app">
-            <div className="home">
+            <div className="home-banner">
                 <div className="info">
                     <div className="hello">
                         <span>Olá, meu nome é</span>
@@ -37,6 +37,68 @@ function main() {
                         <Laptop className="lap" style={{ width: '15vw', height: '30vh' }} />
 
                         <Cellphone className="cell" style={{ width: '15vw', height: '30vh' }} />
+                    </div>
+                </div>
+            </div>
+            <div className="home">
+                <span className="title">O Que Eu Desenvolvo</span>
+                <br />
+                <div className="cards-display">
+                    <div className="card web">
+                        <div className="icon">
+                            <Laptop className="lap" style={{ width: '3pc', height: '3pc' }} />
+                        </div>
+                        <div className="info">
+                            <span className="title">Web</span>
+                            <span className="text">
+                                Desenvolvimento de aplicações web utilizando tecnologias modernas e frameworks populares.
+                            </span>
+                            <span className="link">
+                                Ver Projetos ➡️
+                            </span>
+                        </div>
+                    </div>
+                    <div className="card mobile">
+                        <div className="icon">
+                            <Laptop className="lap" style={{ width: '3pc', height: '3pc' }} />
+                        </div>
+                        <div className="info">
+                            <span className="title">Mobile</span>
+                            <span className="text">
+                                Desenvolvimento de aplicações mobile utilizando tecnologias modernas e frameworks populares.
+                            </span>
+                            <span className="link">
+                                Ver Projetos ➡️
+                            </span>
+                        </div>
+                    </div>
+                    <div className="card desktop">
+                        <div className="icon">
+                            <Laptop className="lap" style={{ width: '3pc', height: '3pc' }} />
+                        </div>
+                        <div className="info">
+                            <span className="title">Desktop</span>
+                            <span className="text">
+                                Desenvolvimento de aplicações desktop utilizando tecnologias modernas e frameworks populares.
+                            </span>
+                            <span className="link">
+                                Ver Projetos ➡️
+                            </span>
+                        </div>
+                    </div>
+                    <div className="card contact">
+                        <div className="icon">
+                            <Laptop className="lap" style={{ width: '3pc', height: '3pc' }} />
+                        </div>
+                        <div className="info">
+                            <span className="title">Contato</span>
+                            <span className="text">
+                                Gostou da plataforma, entre em contato comigo para que possamos conversar sobre o seu projeto.
+                            </span>
+                            <span className="link">
+                                Fale Conosco ➡️
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>

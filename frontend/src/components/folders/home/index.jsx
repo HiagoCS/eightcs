eb5678996c065a545a018ff7c7adc0d6cd90a2cf
HiagoCS <hiago8cs@gmail.com>
2026-08-07 13:03:16 -0300

    responsive home-banner

--- frontend/src/components/folders/home/index.jsx
+++ frontend/src/components/folders/home/index.jsx
@@ -1,5 +1,6 @@
 import './style.scss';
 import Download from '@/assets/icons/download.svg?react';
+import Project from '@/assets/icons/project.svg?react';
 import Laptop from '@/assets/icons/laptop-outline.svg?react';
 import Cellphone from '@/assets/icons/cellphone.svg?react';
 function main(){
@@ -17,11 +18,14 @@ function main(){
                 </div>
                 <div className="buttons">
                     <span className="all">
-                        <button>Ver Projetos</button>
+                        <button>
+                            <Project className="icon" style={{ width: '1.2pc', height: '1.5pc' }} />
+                            Ver Projetos
+                        </button>
                     </span>
                     <span className="cv">
-                        <button>
-                            <Download style={{ width: '1.2pc', height: '1.5pc' }} />
+                        <button title="Baixar Currículo">
+                            <Download className="icon" style={{ width: '1.2pc', height: '1.5pc' }} />
                             Baixar Currículo
                         </button>
                     </span>
@@ -34,14 +38,6 @@ function main(){
                     <Cellphone className="cell" style={{ width: '15vw', height: '30vh' }} />
                 </div>
             </div> 
-            {/* <div className="images">
-                <div className="icon lap">
-                    <Laptop style={{ width: '5vw', height: '30vh' }} />
-                </div>
-                <div className="icon cell">
-                    <Cellphone style={{ width: '5vw', height: '30vh' }} />
-                </div>
-            </div> */}
         </div>
     )
 }

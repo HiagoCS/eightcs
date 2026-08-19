e1ff7c619bee2b668c37d83aeb21a8d37f5efa80
HiagoCS <hiago8cs@gmail.com>
2026-08-19 13:30:58 -0300

    contacts icons moved to an external function

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import Whatsapp from '@/assets/icons/whatsapp-svgrepo-com.svg?react';
-import Linkedin from '@/assets/icons/linkedin-svgrepo-com.svg?react';
-import Github from '@/assets/icons/github-142-svgrepo-com.svg?react';
+
 import Home from '@/assets/icons/home-1-svgrepo-com.svg?react';
 import HomeActive from '@/assets/icons/home-page-svgrepo-com.svg?react';
 import Menu from '@/assets/icons/menu-rounded.svg?react';
 import { data } from '@/data/links';
+import contacts from '@/data/contacts/index.json';
+import { contactIcons } from '@/data/contacts/functions.jsx';
 import { useState } from 'react';
 
 import './style.scss';
@@ -21,7 +21,7 @@ function main() {
                         <ul className='nav' >
                             {data.map((link) => (
                                 <li key={link.url}>
-                                    <Link to={link.url} className={activeLink === link.url ? 'active' : ''} onClick={() => {setActiveLink(link.url); setToggleMenu('')}}>
+                                    <Link to={link.url} className={activeLink === link.url ? 'active' : ''} onClick={() => { setActiveLink(link.url); setToggleMenu('') }}>
                                         {link.url === '/' ?
                                             (activeLink === link.url ?
                                                 <HomeActive style={{ width: '1.3pc', height: '2pc' }} />
@@ -49,9 +49,25 @@ function main() {
                     ))}
                 </ul>
                 <ul className='icons'>
-                    <p><Whatsapp style={{ width: '2.5pc', height: '2.5pc' }} /></p>
+                    {contacts.map(({ id, icon, url }) => {
+                        const Component = contactIcons[icon];
+
+                        return (
+                            <li key={id}>
+                                <a href={url}>
+                                    <Component
+                                        style={{
+                                            width: '2.5pc',
+                                            height: '2.5pc'
+                                        }}
+                                    />
+                                </a>
+                            </li>
+                        );
+                    })}
+                    {/* <p><Whatsapp style={{ width: '2.5pc', height: '2.5pc' }} /></p>
                     <p><Linkedin style={{ width: '3.1pc', height: '3.1pc' }} /></p>
-                    <p><Github style={{ width: '2.5pc', height: '2.5pc' }} /></p>
+                    <p><Github style={{ width: '2.5pc', height: '2.5pc' }} /></p> */}
                 </ul>
             </div>
         </>
---
+++ frontend/src/data/contacts/functions.jsx
@@ -0,0 +1,9 @@
+import Whatsapp from '@/assets/icons/whatsapp-svgrepo-com.svg?react';
+import Linkedin from '@/assets/icons/linkedin-svgrepo-com.svg?react';
+import Github from '@/assets/icons/github-142-svgrepo-com.svg?react';
+
+export const contactIcons = {
+    whatsapp: Whatsapp,
+    linkedin: Linkedin,
+    github: Github
+};

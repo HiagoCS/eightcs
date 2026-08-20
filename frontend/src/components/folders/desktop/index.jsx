c7cd2f9f990f2ab40d6174405eeb436817375ece
HiagoCS <hiago8cs@gmail.com>
2026-08-19 22:47:06 -0300

    copy cards solution to desktop projects

--- frontend/src/components/folders/desktop/index.jsx
+++ frontend/src/components/folders/desktop/index.jsx
@@ -1,6 +1,30 @@
+import "./style.scss"
+import { useState } from "react";
+import DesktopCard from "./cards/index"
+import projects from "@/data/projects/desktop-projects.json"
+import DesktopModal from "./modal/index";
 function main(){
+    const [selectedProject, setSelectedProject] = useState(null);
     return(
-        <h1>desktop</h1>
+        <div className="app" id="desktop-section">
+            
+            {
+                
+                projects.map((project) =>(
+                    <DesktopCard
+                    key={project.id}
+                    project={project}
+                    onClick={() => setSelectedProject(project)}/>
+                ))
+            }
+            {
+                selectedProject &&
+                <DesktopModal 
+                    project={selectedProject}
+                    onClose={() => setSelectedProject(null)}
+                />
+            }
+        </div>
     )
 }
 

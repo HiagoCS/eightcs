8af763c252f1dccfec2a069eb7c2bb115c1a54af
HiagoCS <hiago8cs@gmail.com>
2026-08-22 02:24:38 -0300

    adjust for testing data

--- backend/src/db/seeds/navbarLinks/pages.ts
+++ backend/src/db/seeds/navbarLinks/pages.ts
@@ -4,10 +4,11 @@ function pagesSeed(){
     const insert = sqlite.prepare(`INSERT OR IGNORE INTO navbar_links(id, label, function, url, type_id) VALUES (?, ?, ?, ?, ?)`);
     const pages = [
         [1, "Início", "HomePage", "/"],
-        [2, "Web", "WebPage", "/apps/web", 1],
-        [3, "Mobile", "MobilePage", "/apps/mobile", 2],
-        [4, "Desktop", "DesktopPage", "/apps/desktop", 3],
-        [5, "Contato", "ContactPage", "/contact"]
+        [2, "Contato", "ContactPage", "/contact"],
+        [3, "Web", "WebPage", "/apps/web", 1],
+        [4, "Mobile", "MobilePage", "/apps/mobile", 2],
+        [5, "Desktop", "DesktopPage", "/apps/desktop", 3]
+        
     ];
 
     pages.map((data) =>{

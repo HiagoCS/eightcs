de5ba93f9bb3de5efdfd33200d240b985c498e74
HiagoCS <hiago8cs@gmail.com>
2026-09-16 11:26:43 -0300

    cors configuration on server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,16 @@
 import Fastify = require("fastify");
+import cors = require("@fastify/cors");
 const getRoute = require("./routes/get/index");
 
 
 const server = Fastify({
     logger: true
 });
-
+server.register(cors, {origin:true})
 server.register(getRoute, {prefix:"/api"});
 
 
 server.listen({
-    port: 3000
+    port: 3000,
+    host:"localhost"
 });

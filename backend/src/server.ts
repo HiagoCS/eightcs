263211306c038bf0749a64f61d798c166bdf5661
HiagoCS <hiago8cs@gmail.com>
2026-09-20 15:09:20 -0300

    set storage backend folder to api url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,18 @@
 import Fastify = require("fastify");
 import cors = require("@fastify/cors");
+import path = require("node:path");
+import fastifyStatic = require("@fastify/static");
 const getRoute = require("./routes/get/index");
-
 
 const server = Fastify({
     logger: true
 });
 server.register(cors, {origin:true})
 server.register(getRoute, {prefix:"/api"});
-
+server.register(fastifyStatic, {
+    root: path.join(process.cwd(), "storage"),
+    prefix: "/storage/",
+});
 
 server.listen({
     port: 3000,
